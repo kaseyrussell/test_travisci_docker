378a9b826561d6454dafe0c22ed0b2d3fafb8d65
kaseyrussell <kasey.russell@gmail.com>
2016-09-20 17:02:26 -0400

    added elasticsearch container and test

---
+++ test-es.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+expected='{
+  "name" : "Richard Parker",
+  "cluster_name" : "elasticsearch",
+  "version" : {
+    "number" : "2.2.0",
+    "build_hash" : "8ff36d139e16f8720f2947ef62c8167a888992fe",
+    "build_timestamp" : "2016-01-27T13:32:39Z",
+    "build_snapshot" : false,
+    "lucene_version" : "5.4.1"
+  },
+  "tagline" : "You Know, for Search"
+}'
+actual=`curl -XGET localhost:19200`
+echo "Expecting: $expected"
+echo "Server says: $actual"
+if [ "$expected" != "$actual" ]; then
+  echo "Test failed"
+  exit 1
+else
+  echo "Test passed"
+  exit 0
+fi
+
