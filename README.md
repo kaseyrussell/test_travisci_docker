3eb7ba0acdea6abafcffc893267b840060d22746
kaseyrussell <kasey.russell@gmail.com>
2016-09-20 11:33:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# test_travisci_docker
+
+Copy-pasted verbatim from here:
+https://mike42.me/blog/how-to-set-up-docker-containers-in-travis-ci
+
+just to verify that this works on current Travis-ci system
---
+++ test-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+expected="Hello World"
+actual=`nc -v 192.168.99.100 5000`
+echo "Expecting: $expected"
+echo "Server says: $actual"
+if [ "$expected" != "$actual" ]; then
+  echo "Test failed"
+  exit 1
+else
+  echo "Test passed"
+  exit 0
+fi
+
